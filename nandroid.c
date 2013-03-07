88da3520faac29ec928f3a5b1f702530a0ebe645
Shkerzy <niksamnoy@gmail.com>
2013-03-07 21:01:12 +0800

    Add support /custpack
    Change-Id: I08a0995a85995f72f609ba95236585eefe156ff5

--- nandroid.c
+++ nandroid.c
@@ -353,6 +353,9 @@ int nandroid_backup(const char* backup_path)
     if (0 != (ret = nandroid_backup_partition(backup_path, "/system")))
         return ret;
 
+    if (0 != (ret = nandroid_backup_partition(backup_path, "/custpack")))
+        return ret;
+
     if (0 != (ret = nandroid_backup_partition(backup_path, "/data")))
         return ret;
 
@@ -621,7 +624,7 @@ int nandroid_restore_partition(const char* backup_path, const char* root) {
     return nandroid_restore_partition_extended(backup_path, root, 1);
 }
 
-int nandroid_restore(const char* backup_path, int restore_boot, int restore_system, int restore_data, int restore_cache, int restore_sdext, int restore_wimax)
+int nandroid_restore(const char* backup_path, int restore_boot, int restore_system, int restore_data, int restore_cache, int restore_sdext, int restore_wimax, int restore_custpack)
 {
     ui_set_background(BACKGROUND_ICON_INSTALLING);
     ui_show_indeterminate_progress();
@@ -674,6 +677,9 @@ int nandroid_restore(const char* backup_path, int restore_boot, int restore_syst
     if (restore_system && 0 != (ret = nandroid_restore_partition(backup_path, "/system")))
         return ret;
 
+    if (restore_custpack && 0 != (ret = nandroid_restore_partition(backup_path, "/custpack")))
+        return ret;
+
     if (restore_data && 0 != (ret = nandroid_restore_partition(backup_path, "/data")))
         return ret;
         
@@ -724,7 +730,7 @@ int nandroid_main(int argc, char** argv)
     {
         if (argc != 3)
             return nandroid_usage();
-        return nandroid_restore(argv[2], 1, 1, 1, 1, 1, 0);
+        return nandroid_restore(argv[2], 1, 1, 1, 1, 1, 0, 1);
     }
     
     return nandroid_usage();
